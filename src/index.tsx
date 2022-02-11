d8c198634f1976ae3a3e5a3a82e7d285c57c6f1f
Orr Epstein <orrepstein@gmail.com>
2022-02-11 15:57:01 -0700

    project template

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from 'solid-js/web';
+import 'tailwindcss/tailwind.css';
+
+import './index.css';
+import App from './App';
+
+render(() => <App />, document.getElementById('root') as HTMLElement);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,js,ts,jsx,tsx}'
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
